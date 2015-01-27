36d4a11914873a537408ae43beb35cd3cee1a6eb
lkbhas <lbhasemeyer@gmail.com>
2015-01-27 09:54:10 -0700

    delete and validations - require body work

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -1,16 +1,17 @@
 import Ember from "ember";
-// import EmberValidations from 'ember-validations';
 
 export default Ember.ArrayController.extend({
-// export default Ember.Controller.extend(EmberValidations.Mixin, {
   actions: {
     newNote: function() {
       var body = this.get('noteCopy');
 
-      var note = this.store.createRecord('note', { body: body });
+      if (body && body.trim()) {
+        var note = this.store.createRecord('note', { body: body });
+        note.save();
+      }
       this.set('noteCopy', '');
-      note.save();
     },
+
     deleteNote: function (id) {
       var note = this.store.find('note', id).then(function(note) {
         note.deleteRecord();
@@ -18,10 +19,5 @@ export default Ember.ArrayController.extend({
       });
     }
   }
-  // validations: {
-  //   stuff: {
-  //     presence: true,
-  //     length: { minimum: 5, maximum: 10 }
-  //   }
-  // }
+
 });
